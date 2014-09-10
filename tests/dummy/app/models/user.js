acbe808fbecdc1a48f709d3a5c9175dca40daea8
zhanyulong <zhanyuilong@gmail.com>
2014-09-10 16:41:35 +0800

    add email format validation

--- tests/dummy/app/models/user.js
+++ tests/dummy/app/models/user.js
@@ -3,15 +3,17 @@ import DS from 'ember-data';
 
 export default DS.Model.extend(Ember.Validations.Mixin, {
   login: DS.attr('string'),
-  age: DS.attr('number'),
+  email: DS.attr('string'),
 
   validations: {
     login: {
       presence: true,
       length: { minimum: 5 }
     },
-    age: {
 
+    email: {
+      presence: true,
+      format: { with: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i }
     }
   }
 });
