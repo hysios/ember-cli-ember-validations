911cf9474210333a80bd3f58f536976280a1a7e1
Hysios Hu <hysios@gmail.com>
2014-09-10 14:06:28 +0800

    add numericality validate test

---
+++ tests/unit/components/validate-with-test.js
@@ -0,0 +1,114 @@
+import Ember from 'ember';
+import { test, moduleForModel, moduleForComponent } from 'ember-qunit';
+
+moduleForComponent('validate-with', 'ValidateWithComponent', {
+  // specify the other units that are required for this test
+  //
+  needs: [ 'component:validate-message' ]
+
+});
+
+var SampleModel = Ember.Object.extend(Ember.Validations.Mixin, {
+  field: null,
+  age: null,
+
+  validations: {
+    field: {
+      presence: true
+    },
+    age: {
+      numericality: true
+    }
+  }
+});
+
+var MyTest = Ember.lookup.MyTest = Ember.Namespace.create();
+
+MyTest.sampleModel = SampleModel.create({
+  field: 'hello'
+});
+
+test('it renders', function() {
+  expect(2);
+
+  // creates the component instance
+  var component = this.subject();
+  equal(component._state, 'preRender');
+
+  // appends the component to the page
+  this.append();
+  equal(component._state, 'inDOM');
+});
+
+
+test('it input', function() {
+
+  var component = this.subject({
+    template: Ember.Handlebars.compile('{{input valueBinding="MyTest.sampleModel.field"}}'),
+    propertyBinding: Ember.Binding.from("MyTest.sampleModel.field").to("property")
+  });
+
+  var $component = this.append();
+
+  Ember.run(function() {
+    MyTest.sampleModel.set('field', 'world');
+  });
+
+  equal($component.find('input').length, 1);
+  equal($component.find('input').val(), 'world');
+});
+
+test('it input clear value', function() {
+
+  var component = this.subject({
+    template: Ember.Handlebars.compile(
+      '{{input valueBinding="MyTest.sampleModel.field"}}' +
+      '{{validate-message}}' ),
+    propertyBinding: Ember.Binding.from("MyTest.sampleModel.field").to("property")
+  });
+
+  var $component = this.append();
+
+  Ember.run(function() {
+    MyTest.sampleModel.set('field', '');
+  });
+
+
+  var yieldViews = component._childViews[0],
+      errorMessageView = yieldViews._childViews[1];
+
+  equal(errorMessageView.get('fullMessage'), "can't be blank");
+
+});
+
+test('numericality rule', function() {
+
+  var component = this.subject({
+    template: Ember.Handlebars.compile(
+      '{{input valueBinding="MyTest.sampleModel.age"}}' +
+      '{{validate-message}}' ),
+    propertyBinding: Ember.Binding.from("MyTest.sampleModel.age").to("property")
+  });
+
+  var $component = this.append();
+
+  Ember.run(function() {
+    MyTest.sampleModel.set('age', 'abc');
+  });
+
+
+  var yieldViews = component._childViews[0],
+      errorMessageView = yieldViews._childViews[1];
+
+  equal(errorMessageView.get('fullMessage'), "is not a number");
+
+
+  Ember.run(function() {
+    MyTest.sampleModel.set('age', '123');
+  });
+
+  // yieldViews = component._childViews[0],
+  //     errorMessageView = yieldViews._childViews[1];
+
+  equal(errorMessageView.get('fullMessage'), "");
+});
