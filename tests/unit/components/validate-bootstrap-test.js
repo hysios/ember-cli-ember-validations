8d2784d4620e81d9b59a92a48015fcd4570b1871
zhanyulong <zhanyuilong@gmail.com>
2014-09-11 11:22:04 +0800

    add test for validate-bootstrap component

--- tests/unit/components/validate-bootstrap-test.js
+++ tests/unit/components/validate-bootstrap-test.js
@@ -6,6 +6,28 @@ moduleForComponent('validate-bootstrap', 'ValidateBootstrapComponent', {
   // needs: ['component:foo', 'helper:bar']
 });
 
+
+var SampleModel = Ember.Object.extend(Ember.Validations.Mixin, {
+  field: null,
+  age: null,
+
+  validations: {
+    field: {
+      presence: true
+    },
+    age: {
+      numericality: true
+    }
+  }
+});
+
+var MyTest = Ember.lookup.MyTest = Ember.Namespace.create();
+
+MyTest.bootstrapModel = SampleModel.create({
+  field: 'hello'
+});
+
+
 test('it renders', function() {
   expect(2);
 
@@ -20,3 +42,42 @@ test('it renders', function() {
   this.append();
   equal(component.state, 'inDOM');
 });
+
+test('it returns different layoutName', function() {
+  var component = this.subject();
+
+  equal(component.get('layoutName'), 'components/validate-bootstrap-default');
+
+  Ember.run(function() {
+    component.set('horizontal', true);
+  });
+
+  equal(component.get('layoutName'), 'components/validate-bootstrap-horizontal');
+});
+
+
+test('default validateStatus value should be empty a string', function() {
+  var component = this.subject({
+    canValidate: false
+  });
+
+  equal(component.get('validateStatus'), '');
+});
+
+
+test('validateStatus depends on the value of isvalid', function() {
+  var component = this.subject({
+    canValidate: true
+  });
+
+  Ember.run(function() {
+    component.set('isValid', true);
+  });
+  equal(component.get('validateStatus'), 'has-success');
+
+  Ember.run(function() {
+    component.set('isValid', false);
+  });
+  equal(component.get('validateStatus'), 'has-error');
+});
+
