03bacb2637690cfca3d78984343b9177add44811
gy <475347159@qq.com>
2014-09-11 09:50:30 +0800

    unit test for format

--- tests/dummy/app/models/user.js
+++ tests/dummy/app/models/user.js
@@ -8,7 +8,9 @@ export default DS.Model.extend(Ember.Validations.Mixin, {
   validations: {
     login: {
       presence: true,
-      length: { minimum: 5 }
+      length: { minimum: 5 , maximum: 15 },
+      format: { with: /^([a-zA-Z]|\d)+$/,
+       message: 'must be letters and numbers only'  }
     },
     age: {
 
