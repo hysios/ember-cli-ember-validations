7053ea8f559c80dabcec69dd245fbf40a2e48ae2
zhangrui <517144093@qq.com>
2014-09-11 15:43:38 +0800

    add select and checkbox validation

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   user: function(){
     return this.store.createRecord('user');
+  }.property(),
+
+  options: function(){
+    return ['不及格', '及格', '良好', '优秀'];
   }.property()
 });
--- tests/dummy/app/models/user.js
+++ tests/dummy/app/models/user.js
@@ -4,6 +4,8 @@ import DS from 'ember-data';
 export default DS.Model.extend(Ember.Validations.Mixin, {
   login: DS.attr('string'),
   email: DS.attr('string'),
+  level: DS.attr('string'),
+  agreement: DS.attr('boolean'),
 
   validations: {
     login: {
@@ -15,6 +17,14 @@ export default DS.Model.extend(Ember.Validations.Mixin, {
     email: {
       presence: true,
       format: { with: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i }
+    },
+
+    level: {
+      presence: true
+    },
+    agreement: {
+      acceptance: true
     }
   }
+
 });
