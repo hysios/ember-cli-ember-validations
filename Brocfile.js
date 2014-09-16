10450a91ff26ed0f793bc54463bfb86c9365dbc9
gy <475347159@qq.com>
2014-09-16 16:44:04 +0800

    select style ugly in ie and firefox

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,7 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
-
+app.import('bower_components/respond/dest/respond.min.js');
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
